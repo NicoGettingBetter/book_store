b78fd077a1581f5ffe2775db0ca8cadcd66c968c
NicoGettingBetter <nicolas.ozz.com@gmail.com>
2016-07-27 12:56:56 +0300

    add cancan

---
+++ app/models/ability.rb
@@ -0,0 +1,17 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    can :read, [Book, Review, Category, Author]
+    if user
+      can :read, [Order, User], user_id: user.id
+      can :create, Review
+      can :manage, [Order, OrderItem, User], user_id: user.id
+      if user.admin?
+        can :access, :rails_admin
+        can :dashboard
+        can :manage, [Order, Book, Review, Category, Author, Delivery, Coupon]
+      end
+    end
+  end
+end
