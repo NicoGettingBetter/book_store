e15b06db46c55ce6792f2a5dea8d7ee9cdcb2a03
Nicolas Saenko <nicolas.ozz.com@gmail.com>
2016-08-09 18:34:10 +0300

    add authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,23 @@ class OrdersController < ApplicationController
   include Rectify::ControllerHelpers
   before_action :set_order, only: [:destroy, :complete]
   before_action :set_presenter, except: :index
+  load_and_authorize_resource
   helper AddressHelper
 
   def index
-    redirect_to root_path if cannot? :read, Order
   end
 
   def show
-    redirect_to root_path if cannot? :read, Order
   end
 
   def edit
-    redirect_to root_path if cannot? :manage, Order
   end
 
   def update
-    redirect_to root_path if cannot? :manage, Order
     @form = OrderForm.from_params(order_params, order_items: order_items_params)
     validate = [:order_items]
     validate << :coupon if order_params[:coupon] != ''
-    
+
     UpdateOrder.call(@form, validate) do
       on(:ok) { redirect_to edit_order_path(id: current_order.id) }
       on(:invalid) { render 'edit' }
@@ -29,11 +26,9 @@ class OrdersController < ApplicationController
   end
 
   def edit_address
-    redirect_to root_path if cannot? :manage, Order
   end
 
   def order_address
-    redirect_to root_path if cannot? :manage, Order
     @form = OrderForm.new(billing_address: AddressForm.from_params(address_params(:billing_address),
       type: :billing_address))
     @form.same_address = params.require(:use_billing_address).permit(:check)[:check] == '1'
@@ -49,12 +44,10 @@ class OrdersController < ApplicationController
   end
 
   def edit_delivery
-    redirect_to root_path if cannot? :manage, Order
     presenter.deliveries = Delivery.all
   end
 
   def order_delivery
-    redirect_to root_path if cannot? :manage, Order
     @form = OrderForm.new(delivery: DeliveryForm.new(id: delivery_params.to_i))
     presenter.deliveries = Delivery.all if @form.invalid?
 
@@ -65,12 +58,10 @@ class OrdersController < ApplicationController
   end
 
   def edit_payment
-    redirect_to root_path if cannot? :manage, Order
     presenter.credit_card = CreditCardForm.from_model(presenter.order.credit_card)
   end
 
   def order_payment
-    redirect_to root_path if cannot? :manage, Order
     @form = OrderForm.new(credit_card: CreditCardForm.new(credit_card_params))
     add_credit_card_to_presenter if @form.invalid?
 
@@ -81,11 +72,9 @@ class OrdersController < ApplicationController
   end
 
   def edit_confirm
-    redirect_to root_path if cannot? :manage, Order
   end
 
   def order_confirm
-    redirect_to root_path if cannot? :manage, Order
     @form = OrderForm.new()
     UpdateOrder.call(@form, :order) do
       on(:ok) { redirect_to complete_order_path }
@@ -93,11 +82,9 @@ class OrdersController < ApplicationController
   end
 
   def complete
-    redirect_to root_path if cannot? :read, Order
   end
 
   def destroy
-    redirect_to root_path if cannot? :manage, Order
     OrderItem.delete @order.order_items
     @order.delete
     respond_to do |format|
